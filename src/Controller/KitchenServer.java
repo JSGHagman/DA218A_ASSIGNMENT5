821eedfdfc6ddcf609ad0ea27eef489fefdb3fa6
Jakob Hagman <93573152+JSGHagman@users.noreply.github.com>
2022-05-25 17:08:40 +0200

    Commit

---
+++ src/Controller/KitchenServer.java
@@ -0,0 +1,25 @@
+package Controller;
+import java.util.concurrent.CompletableFuture;
+
+public class KitchenServer extends AbstractKitchenServer{
+
+    @Override
+    public CompletableFuture<KitchenStatus> receiveOrder(Order order) throws InterruptedException {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<OrderStatus> checkStatus(String orderID) throws InterruptedException {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<KitchenStatus> serveOrder(String orderID) throws InterruptedException {
+        return null;
+    }
+
+    @Override
+    protected void cook(Order order) {
+
+    }
+}
---
+++ src/Controller/OrderClient.java
@@ -0,0 +1,19 @@
+package Controller;
+
+public class OrderClient extends AbstractOrderClient{
+
+    @Override
+    public void submitOrder() {
+
+    }
+
+    @Override
+    protected void startPollingServer(String orderId) {
+
+    }
+
+    @Override
+    protected void pickUpOrder() {
+
+    }
+}
