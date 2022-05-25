c974f036139acb7690dee7d4dd0d2884d96b07d5
Jakob Hagman <93573152+JSGHagman@users.noreply.github.com>
2022-05-25 16:58:16 +0200

    Commit

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         GenericRestaurantForm restaurant = new GenericRestaurantForm();
         restaurant.Start();
-//        MainForm form = new MainForm();
+        System.out.println("Assignment 5");
+        //        MainForm form = new MainForm();
     }
 }
